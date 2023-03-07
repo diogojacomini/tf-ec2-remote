04881d2e15cc5d56649049ff6e3c8cf2ea1467d8
Diogo <diogo.jacomini2@gmail.com>
2023-03-07 11:16:14 -0300

    fix: ordem for destroy.

--- run_destroy_all.sh
+++ run_destroy_all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "destroy bucket-state"
-cd ./bucket-state
-terraform destroy
-
-echo "destroy vpc-network"
-cd ../vpc-network
-terraform destroy
-
 echo "destroy EC2"
 terraform destroy
 
 echo "delete key"
 rm *key*
+
+echo "destroy vpc-network"
+cd ./vpc-network
+terraform destroy
+
+echo "destroy bucket-state"
+cd ../bucket-state
+terraform destroy
